ad732c258b22ce6efbeddaf4c54ef3ef0db04b30
Oscar Gonzalez <wacry77@gmail.com>
2025-04-09 16:10:34 -0300

    API Rest de market

---
+++ src/main/java/com/platzi/platzi_market/PlatziMarketApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.platzi_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMarketApplication.class, args);
+	}
+
+}
